2a3fbe139d1b9d5abbb8417b1f82704ac079d0f5
“Sophia” <jungwon.lim.0210@gmail.com>
2017-11-21 14:58:18 +0200

    found wikipedia api and we will manipulate it for our database

---
+++ playground/data.js
@@ -0,0 +1,19 @@
+// <script src="http://en.wikipedia.org/w/api.php?action=opensearch&limit=10&format=json&callback=getData&search="></script>
+//
+//
+// <input type="text" id="searchbox">
+// <div id="output"></div>
+//this part will go to html file later
+
+function getData(data) {
+  document.getElementById('output').innerHTML = data;
+}
+
+var myScript = '';
+document.getElementById('searchbox').onkeyup = function(){
+  var searchText = document.getElementById('searchbox').value;
+  myScript = document.createElement('script');
+  myScript.src = "http://en.wikipedia.org/w/api.php?action=opensearch&limit=10&format=json&callback=getData&search=" + searchText;
+
+document.body.appendChild(myScript);
+};
