090fb569d1f4f3ed76b01935bd0754be4512145b
Idan Tabachnik <idantb101@gmail.com>
2017-11-22 15:31:27 +0200

    the not working version there is no issue and there is no reason for one because nothing is working so we are stoping to ake some research

--- src/handlers.js
+++ src/handlers.js
@@ -1,2 +1,64 @@
-const path = require('path');
+const data = require('./model');
 const fs = require('fs');
+const path = require('path');
+
+
+// function setFileName(endPoint) {
+//     if (endPoint === '/') {
+//         return 'index.html';
+//     }
+// }
+
+// function setHeaders(request) {
+//     const extension = request.url.split('.')[1];
+//     const extensionType = {
+//         'html': 'text/html',
+//         'css': 'text/css',
+//         'js': 'javascript/application'
+//     }[extension];
+//     return extensionType;
+// }
+const handlers = {};
+
+handlers.home = function (request, response) {
+    const endpoint = request.url;
+    if (endpoint === '/') {
+        fs.readFile(__dirname + '/../public/index.html',
+            function (error, file) {
+                if (error) {
+                    console.log(request.url);
+                    console.log('oh there is an error', error);
+                    return;
+                }
+                response.writeHead(200, { 'Content-Type': 'text/html' });
+                response.end(file);
+            });
+    }else{
+        // var header = setHeaders(request);
+        const extension = request.url.split('.')[1];
+        const extensionType = {
+            'html': 'text/html',
+            'css': 'text/css',
+            'js': 'javascript/application'
+        }[extension];
+        
+        fs.readFile(__dirname + '/../public/'+ request.url,
+        function (error, file) {
+            if (error) {
+                console.log(request.url);
+                console.log('oh there is an error', error);
+                return;
+            }
+            response.writeHead(200, { 'Content-Type': extensionType });
+            response.end(file);
+        });
+    }
+}
+
+
+handlers.notFound = function (request, response) {
+    response.writeHead(404, { 'Content-Type': 'text/html' });
+    response.end('Resources not found');
+}
+module.exports = handlers;
+
--- src/server.js
+++ src/server.js
@@ -1,2 +1,10 @@
 const http = require("http");
 const router = require('./router');
+
+var port = process.env.port || 9000;
+var app = function (request, response) {
+    router(request, response);
+};
+
+http.createServer(app).listen(port);
+console.log('server listening on port', port);
