cfd4479bc1336a2e2ca4b06e3e70b3387cdfbd7e
Idan Tabachnik <idantb101@gmail.com>
2017-11-23 09:04:06 +0200

    Handle data from API request in the client side #32

--- public/logic.js
+++ public/logic.js
@@ -1,21 +1,32 @@
-(function onLoad(){
-    document.getElementById('searchQuery').addEventListener(
-        'input',function(event){
-           var searchQuery = event.target.value;
-           sendReqToServer(searchQuery);
-        });
+(function onLoad() {
+  document.getElementById('searchQuery').addEventListener(
+    'input', function (event) {
+      var searchQuery = event.target.value;
+      sendReqToServer(searchQuery, apiCallback);
+    });
 
-        function sendReqToServer (searchQuery){
-          var xhr = new XMLHttpRequest();
-          xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-             console.log(xhr.responseText);
-            }else{
-              console.log(xhr);
-            }
-          }
-          xhr.open("GET",window.location.href + "model.js");
-          xhr.send();
-        };
+  function apiCallback(apiResults) {
+    var ulElement = document.getElementById('searchResult');
+    apiResults.forEach(function (apiResult) {
+      var listElement = document.createElement('li');
+      var linkElement = document.createElement('a');
+      var linkName = document.createTextNode(apiResult.title);
+      linkElement.href = apiResult.link;
+      linkElement.appendChild(linkName);
+      listElement.appendChild(linkElement);
+      ulElement.appendChild(listElement);
+    });
+  }
 
-    }());
+  function sendReqToServer(searchQuery, apiCallback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        apiCallback(JSON.parse(xhr.responseText));
+      }
+    }
+    xhr.open("GET", window.location.href + "model.js");
+    xhr.send();
+  };
+
+}());
