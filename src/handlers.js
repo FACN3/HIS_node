ef8ae79d8da00d8ac215eeb6d101f5f4d287c934
Idan Tabachnik <idantb101@gmail.com>
2017-11-23 12:32:27 +0200

    Only one handler handles errors #52 also clearing some comment out code

--- src/handlers.js
+++ src/handlers.js
@@ -10,14 +10,12 @@ handlers.html = function (request, response) {
         fs.readFile(__dirname + '/../public/index.html',
             function (error, file) {
                 if (error) {
-                    console.log(request.url);
-                    console.log('oh there is an error', error);
-                    return;
+                    heandleReadFileError(error);
                 }
                 response.writeHead(200, { 'Content-Type': 'text/html' });
                 response.end(file);
             });
-     }
+    }
 }
 
 handlers.staticFiles = function (request, response) {
@@ -29,31 +27,34 @@ handlers.staticFiles = function (request, response) {
         'js': 'javascript/application'
     }[extension];
 
-    fs.readFile(__dirname + '/../public/'+ request.url,
-    function (error, file) {
-        if (error) {
-            console.log(request.url);
-            console.log('oh there is an error', error);
-            return;
-        }
-        response.writeHead(200, { 'Content-Type': extensionType });
-        response.end(file);
-    });
+    fs.readFile(__dirname + '/../public/' + request.url,
+        function (error, file) {
+            if (error) {
+                heandleReadFileError(error);
+            }
+            response.writeHead(200, { 'Content-Type': extensionType });
+            response.end(file);
+        });
 }
 
 handlers.model = function (request, response) {
-    getData(request.url ,function (error,purpose){  
-        if (error){
-            response.writeHead(404, { 'Content-Type': 'text/html' });
-            response.end('error geting data from wiki', error);   
+    getData(request.url, function (error, purpose) {
+        if (error) {
+            heandleReadFileError(error);
         }
         response.writeHead(200, { 'Content-Type': 'application/json' });
         response.end(JSON.stringify(purpose));
-        });
+    });
 }
 
 handlers.notFound = function (request, response) {
     response.writeHead(404, { 'Content-Type': 'text/html' });
     response.end('Resources not found');
 }
+
+function heandleReadFileError(error) {
+    response.writeHead(404, { 'Content-Type': 'text/html' });
+    response.end('error geting data from wiki', error);
+}
+
 module.exports = handlers;
--- src/model.js
+++ src/model.js
@@ -8,14 +8,6 @@ function getData(searchQuery, callback) {
             return callback('error' ,error);
         }
         var parsedData = JSON.parse(body);
-        // var array = [];
-        // for (var i = 0; i < 10; i++) {
-        //     var object = {
-        //         title: parsedData[1][i],
-        //         link: parsedData[3][i]
-        //     };
-        //     array.push(object);
-        // }
         const array = Array.from({length:10}, (el, index) => {
             return {
               title: parsedData[1][index],
