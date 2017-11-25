4b2e13a639207e83fcf15dcdc38d33cae0997fa4
Idan Tabachnik <idantb101@gmail.com>
2017-11-25 21:25:46 +0200

    html and staticFile code repeats #56
    refactoring handler

--- src/handlers.js
+++ src/handlers.js
@@ -5,29 +5,18 @@ const path = require('path');
 const handlers = {};
 
 handlers.html =  (request, response) => {
-    const endpoint = request.url;
-    if (endpoint === '/') {
-        fs.readFile(__dirname + '/../public/index.html',
-             (error, file) => {
-                if (error) {
-                    heandleReadFileError(error);
-                }
-                response.writeHead(200, { 'Content-Type': 'text/html' });
-                response.end(file);
-            });
-    }
+    handlers.staticFiles(request, response ,'/index.html')
 }
 
-handlers.staticFiles =  (request, response) => {
-    const endpoint = request.url;
-    const extension = request.url.split('.')[1];
+handlers.staticFiles =  (request, response ,url) => {
+    const extension = url.split('.')[1];
     const extensionType = {
         'html': 'text/html',
         'css': 'text/css',
         'js': 'javascript/application'
     }[extension];
 
-    fs.readFile(__dirname + '/../public/' + request.url,
+    fs.readFile(__dirname + '/../public/' + url,
         function (error, file) {
             if (error) {
                 heandleReadFileError(error);
@@ -47,14 +36,9 @@ handlers.model =  (request, response) => {
     });
 }
 
-handlers.notFound =  (request, response) => {
+ handlers.heandleError = (error) => {
     response.writeHead(404, { 'Content-Type': 'text/html' });
-    response.end('Resources not found');
-}
-
- handlers.heandleReadFileError = (error) => {
-    response.writeHead(404, { 'Content-Type': 'text/html' });
-    response.end('error geting data from wiki', error);
+    response.end('Sorry, an error has occurred', error);
 }
 
 module.exports = handlers;
--- src/router.js
+++ src/router.js
@@ -4,15 +4,15 @@ const routes = {
     '/' : handlers.html,
     '/style.css' : handlers.staticFiles,
     '/logic.js'  : handlers.staticFiles,
-    '404' : handlers.notFound
+    '404' : handlers.heandleError
 }
 
 module.exports = (request, response) => {
     if (routes[request.url]) {
-        routes[request.url](request, response);
+        routes[request.url](request, response ,{url});
     }else if (request.url.split('?')[0] === '/model'){
         handlers.model(request, response);
     } else {
-        routes[404](request, response);
+        routes[404]('404, page not found');
     }
 }
